aeef10c6e3e870951e483b6f6ad51eefee59fb3f
Julien Leicher <jleicher@gmail.com>
2012-06-01 19:32:51 +0200

    Added comments and native.c template

---
+++ src/native/nui_template/native.c
@@ -0,0 +1,18 @@
+/**
+ *	Native methods implementation. See nui/native.h for explanation of all methods.
+ */
+
+void nui_native_init(NUI_INIT_ARGS_D)
+{
+
+}
+// -----------------------------------------------------------------
+void nui_native_main_loop()
+{
+
+}
+// -----------------------------------------------------------------
+void nui_native_alert(const char* msg)
+{
+	
+}
